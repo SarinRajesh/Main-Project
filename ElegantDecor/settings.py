607f1c1516de10309e09025c9cb3c9a8a714beeb
SarinRajesh <77143396+SarinRajesh@users.noreply.github.com>
2024-10-25 21:52:52 +0530

    Update settings.py

--- ElegantDecor/settings.py
+++ ElegantDecor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r$-57d+cx^d3ps3#3&_fb6+#!sk%k7ta$e%iwz4k4%=h9cytq#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "elegantdecor.onrender.com"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -162,6 +162,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
