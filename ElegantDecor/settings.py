79f6024eebb92bbb21e6d671c5591f6c4a5a252f
SarinRajesh <sarinrajesh2025@mca.ajce.in>
2025-04-02 12:20:13 +0530

    render error fix

--- ElegantDecor/settings.py
+++ ElegantDecor/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-     'corsheaders',
+     'corsheaders.middleware.CorsMiddleware',
     'allauth',
     'allauth.socialaccount',
     'django.contrib.sites',
